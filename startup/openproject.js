fcf38914af848483ef79f527d20d00ad9b78aa58
KeyDeveloperDoc <keydeveloperdoc@users.noreply.github.com>
2016-12-06 13:12:12 +0100

    File headers

---
+++ startup/openproject.js
@@ -0,0 +1,7 @@
+/* 
+ * INSTALLATION: Copy this script to "<user>/KeyDeveloper/scripting/startup"
+ *
+ * SAMPLE: Open a specific project on startup. Supply an absolute path.
+ */
+
+app.openProject("/projects/demo.project");
